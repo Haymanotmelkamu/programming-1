411abc67408313544b24aeb8ae17cdfd67c66585
Haymanotmelkamu <129688196+Haymanotmelkamu@users.noreply.github.com>
2023-04-10 16:10:22 +0300

    Add files via upload

---
+++ Exercise ch 2.cpp
@@ -0,0 +1,72 @@
+﻿/*#include<iostream>
+using namespace std;
+int main(){
+int x,y,z,result;
+cout<<"enter a number";
+cin>>x>>y>>z;
+result = x*y*z;
+cout<<"the product is"<<result;
+return 0;
+}*/
+
+
+/*#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"enter two number";
+int a,b,sum,dif,pro,div;
+cin>>a>>b;
+sum=a+b;
+dif=a-b;
+pro=a*b;
+div=a/b;
+cout<<sum<<endl<<dif<<endl<<pro<<endl<<div<<endl;
+if(a>b)
+cout<<"a is greater"<<endl;
+else
+cout<<"b is greater"<<endl;
+return 0;
+}*/
+/*#include<iostream>
+using namespace std;
+int main(){
+ float r;
+const float pi=3.146;
+cout<<"enter to radius of the circle"<<endl;
+cin >>r;
+float cir=(2*pi)*r;
+cout<<"the circle with radius of "<<r<<"circum france is"<<cir;
+return 0;
+}*/
+
+  /*#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+ int a,b,c;
+cout<<"enter the value of a "<<endl;
+cin>>a;
+cout<<"enter the value of b"<<endl;
+cin >>b;
+cout<<"enter the value of c"<<endl;
+int x1,x2;
+cin>>c;
+int temp;
+temp=sqrt((b*b)-(4*a*c));
+
+if(temp<0){
+cout<<"the equation does not have solution"<<endl;
+}
+else if(temp==0){
+x1=(-1*b)/(2*a);
+cout<<"the solution is"<<x1<<endl;
+}
+else{
+x1=((-1*b)-temp)/(2*a);
+x2=((-1*b)+temp)/(2*a);
+cout<<"the solution is"<<x1<<endl;
+cout<<"the solution is"<<x2<<endl;
+}
+return 0;
+}*/
